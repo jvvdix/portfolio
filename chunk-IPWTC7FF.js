7ab2754846b9ed1cdf26915c6924ba350c1899e7
jvvdix <judit.molina.98@gmail.com>
2025-06-03 13:03:39 +0200

    Auto-generated commit

---
+++ chunk-IPWTC7FF.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-P46YOMWX.js";import"./chunk-DAQQSBQ4.js";export{a as SkillsComponent};
--- chunk-Y62OONQC.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-UJOQW3YT.js";import"./chunk-DAQQSBQ4.js";export{a as SkillsComponent};
